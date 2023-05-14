474c60fb4d52c291b2e08449cc54bbee3cef0068
koreanbulbasaur <koreanbulbasaur99@naver.com>
2023-05-14 11:13:19 +0900

    just

--- semi_project/Design/2_Main.py
+++ semi_project/Design/2_Main.py
@@ -1,6 +1,14 @@
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
+import matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+matplotlib.rcParams['font.family'] = 'Malgun Gothic'
+matplotlib.rcParams['font.size'] = 15 # 글자크기
+matplotlib.rcParams['axes.unicode_minus']=False
 
 form_class = uic.loadUiType(r'C:\Project\semi_project\Design\2_Main.ui')[0]
 
@@ -12,6 +20,7 @@ class WindowClass(QMainWindow, form_class):
 
         self.open_btn.clicked.connect(self.openFunction)
         self.graph_btn.clicked.connect(self.createGraph)
+
 
     def openFunction(self):
         fname = QFileDialog.getOpenFileName(self)
@@ -28,10 +37,13 @@ class FirstOption(QDialog):
         uic.loadUi(r'C:\Project\semi_project\Design\2_first_option.ui', self)
 
         self.first_next_btn.clicked.connect(self.secondwindow)
+        # self.print_btn.clicked.connect(self.print_data)
 
         self.show()
 
     def secondwindow(self):
+        header_index = self
+        self.reject()
         SecondOption(self)
 
 
@@ -39,10 +51,36 @@ class SecondOption(QDialog):
     def __init__(self, parent):
         super(SecondOption, self).__init__(parent)
         uic.loadUi(r'C:\Project\semi_project\Design\2_second_option.ui', self)
+
+        self.second_next_btn.clicked.connect(self.thirdwindow)
+
         self.show()
 
+    def thirdwindow(self):
+        self.reject()
+        ThirdOption(self)
+
+
+class ThirdOption(QDialog):
+    def __init__(self, parent):
+        super(ThirdOption, self).__init__(parent)
+        uic.loadUi(r'C:\Project\semi_project\Design\2_third_option.ui', self)
+
+        self.complete_btn.clicked.connect(self.create_graph)
+
+
+        self.show()
+
+    def create_graph(self):
+        self.reject()
+        CreateGraph(self)
+
+class CreateGraph():
+    def __init__(self):
+        pass
 
 app = QApplication(sys.argv)
 mainWindow = WindowClass()
 mainWindow.show()
+
 app.exec_()
