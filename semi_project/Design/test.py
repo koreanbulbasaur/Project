b6c0741f06f9fddc227a83737de78878e6d7aa8d
koreanbulbasaur <koreanbulbasaur99@naver.com>
2023-05-14 11:13:23 +0900

    just

--- semi_project/Design/test.py
+++ semi_project/Design/test.py
@@ -1,43 +1,43 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget
+from PyQt5.QtWidgets import QApplication, QDialog, QPushButton, QVBoxLayout
 
 
-class MainWindow(QMainWindow):
+class FirstDialog(QDialog):
     def __init__(self):
         super().__init__()
 
-        self.initUI()
+        self.second_dialog = None
 
-    def initUI(self):
-        self.setWindowTitle('Simple PyQt5 Example')
+        layout = QVBoxLayout()
 
-        vbox = QVBoxLayout()
+        button = QPushButton('Open Second Dialog')
+        button.clicked.connect(self.open_second_dialog)
+        layout.addWidget(button)
 
-        self.label = QLabel('Hello, Wrtn!')
-        vbox.addWidget(self.label)
+        self.setLayout(layout)
 
-        self.edit = QLineEdit()
-        vbox.addWidget(self.edit)
-
-        self.button = QPushButton('Change Text')
-        self.button.clicked.connect(self.change_label_text)
-        vbox.addWidget(self.button)
-
-        container = QWidget()
-        container.setLayout(vbox)
-        self.setCentralWidget(container)
-
-    def change_label_text(self):
-        text = self.edit.text()
-        self.label.setText(text)
+    def open_second_dialog(self):
+        self.second_dialog = SecondDialog(self)
+        self.second_dialog.show()
+        self.reject()
 
 
-def main():
-    app = QApplication(sys.argv)
-    main_window = MainWindow()
-    main_window.show()
-    sys.exit(app.exec_())
+class SecondDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        layout = QVBoxLayout()
+
+        button = QPushButton('Close')
+        button.clicked.connect(self.close)
+        layout.addWidget(button)
+
+        self.setLayout(layout)
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication([])
+
+    first_dialog = FirstDialog()
+    first_dialog.show()
+
+    app.exec_()
