1de1ef4cade3ea5ecfde56294719d97820c19731
koreanbulbasaur <koreanbulbasaur99@naver.com>
2023-05-14 19:45:53 +0900

    table

--- semi_project/Design/2_Main.py
+++ semi_project/Design/2_Main.py
@@ -6,6 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from PyQt5.QtWidgets import QTableWidgetItem, QAbstractItemView
+from PyQt5.QtCore import Qt
 
 matplotlib.rcParams['font.family'] = 'Malgun Gothic'
 matplotlib.rcParams['font.size'] = 15 # 글자크기
@@ -43,42 +44,96 @@ class FirstOption(QDialog):
         super(FirstOption, self).__init__(parent)
         uic.loadUi(r'C:\Project\semi_project\Design\2_first_option.ui', self)
 
+        self.header_Index = None
+        self.index_Col = None
+
         self.first_next_btn.clicked.connect(self.secondwindow)
 
         self.show()
 
     def secondwindow(self):
-        headerIndex = self.Index_Row.text()
-        indexCol = self.Index_Column.text()
+        header_Index = self.Index_Row.text()
+        index_Col = self.Index_Column.text()
+
+        data = self.parent().file_name
 
         self.reject()
-        SecondOption(self)
-
+        SecondOption(self.parent(), data, header_Index, index_Col)
 
 class SecondOption(QDialog):
-    def __init__(self, parent):
+    def __init__(self, parent, data, header_Index, index_Col):
         super(SecondOption, self).__init__(parent)
         uic.loadUi(r'C:\Project\semi_project\Design\2_second_option.ui', self)
 
+        self.firsttable = self.first_table
+        self.secondtable = self.second_table
+
         self.second_next_btn.clicked.connect(self.thirdwindow)
+        self.T_btn.clicked.connect(self.retrieveCheckboxValues)
+
+        self.header_Index = int(header_Index)
+        self.index_Col = index_Col
+
+        self.data = data
+
+        if data.endswith('.csv'):
+            self.df = pd.read_csv(data)
+        elif data.endswith('.xlsx'):
+            # print(self.header_Index)
+            self.df = pd.read_excel(data, index_col=self.header_Index)
+
+        self.firsttable.setRowCount(len(self.df))
+        self.firsttable.setColumnCount(1)
+        self.firsttable.setHorizontalHeaderLabels([self.df.index.name])
+
+        for row in range(len(self.df)):
+            self.df.index = self.df.index.astype('object')
+            item = QTableWidgetItem(str(self.df.index[row]))
+            item.setFlags(Qt.ItemFlag.ItemIsUserCheckable |Qt.ItemFlag.ItemIsEnabled)
+            item.setCheckState(Qt.CheckState.Unchecked)
+            self.firsttable.setItem(row, 0, item)
+
+        self.secondtable.setRowCount(len(self.df.columns))
+        self.secondtable.setColumnCount(1)
+        self.secondtable.setHorizontalHeaderLabels(['열'])
+
+        for col in range(len(self.df.columns)):
+            item = QTableWidgetItem(str(self.df.columns[col]))
+            item.setFlags(Qt.ItemFlag.ItemIsUserCheckable |Qt.ItemFlag.ItemIsEnabled)
+            item.setCheckState(Qt.CheckState.Unchecked)
+            self.secondtable.setItem(col, 0, item)
 
         self.show()
 
     def thirdwindow(self):
         self.reject()
-        ThirdOption(self)
+        ThirdOption(self, self.df)
+
+    def retrieveCheckboxValues(self):
+        for row in range(self.firsttable.rowCount()):
+            if self.firsttable.item(row, 0).checkState() == Qt.CheckState.Checked:
+                print([self.firsttable.item(row, col).text()
+                      for col in range(self.firsttable.columnCount())])
+        print('-'*100)
 
 class ThirdOption(QDialog):
-    def __init__(self, parent):
+    def __init__(self, parent, df):
         super(ThirdOption, self).__init__(parent)
         uic.loadUi(r'C:\Project\semi_project\Design\2_third_option.ui', self)
+
+        self.df = df
 
         self.complete_btn.clicked.connect(self.create_graph)
 
         self.show()
 
     def create_graph(self):
+        row = self.tableWidget.currentRow()
+        col = self.tableWidget.currentColumn()
+        value = self.df.iloc[row, col]
+        # value 값을 이용하여 차트 그리기
         self.reject()
+
 
 app = QApplication(sys.argv)
 mainWindow = WindowClass()
