c6e33a96a2f92b74754b6dcd2bd0e5889cecec10
koreanbulbasaur <koreanbulbasaur99@naver.com>
2023-05-19 12:38:08 +0900

    movefile

---
+++ test/department_store.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
+
+options = webdriver.ChromeOptions()
